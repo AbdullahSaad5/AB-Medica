29f6daaa13c2c999573fd37789375d964c5d27b7
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2025-03-15 21:07:26 +0500

    Remove delay from video index update logic for immediate navigation response

--- app/setup-alt/page.tsx
+++ app/setup-alt/page.tsx
@@ -115,15 +115,22 @@ const Setup = () => {
         className={`absolute inset-0 w-full h-full object-cover ${showingVideo ? "visible" : "invisible"}`}
         playsInline
         muted
-        onPlay={() => {
+        // onPlay={() => {
+        //   if (direction === "forward") {
+        //     setTimeout(() => {
+        //       setCurrentImageIndex((prevIndex) => prevIndex + 1);
+        //     }, 100);
+        //   } else {
+        //     setTimeout(() => {
+        //       setCurrentImageIndex((prevIndex) => prevIndex - 1);
+        //     }, 100);
+        //   }
+        // }}
+        onCanPlayThrough={() => {
           if (direction === "forward") {
-            setTimeout(() => {
-              setCurrentImageIndex((prevIndex) => prevIndex + 1);
-            }, 100);
+            setCurrentImageIndex((prevIndex) => prevIndex + 1);
           } else {
-            setTimeout(() => {
-              setCurrentImageIndex((prevIndex) => prevIndex - 1);
-            }, 100);
+            setCurrentImageIndex((prevIndex) => prevIndex - 1);
           }
         }}
         onEnded={() => {
