99543e295b712b65372d3adcb67738edae9ab7d5
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2025-03-19 08:34:56 +0500

    Update PDF.js worker source to use legacy build for improved compatibility

--- app/_components/PDFViewer/index.tsx
+++ app/_components/PDFViewer/index.tsx
@@ -5,7 +5,8 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
 // Configure PDF.js worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+// pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
 
 interface PDFViewerProps {
   pdfUrl: string;
